a883ae6171452181428bfd619a55b73e6f011716
Pavel <92925215+Pave1Pal@users.noreply.github.com>
2023-10-21 10:58:37 +0300

    feat: sybil-center friendship (#2)

--- components/component.tsx
+++ components/component.tsx
@@ -1,89 +1,110 @@
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { Noir } from "@noir-lang/noir_js";
-import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
-import circuit from "../circuits/target/noirstarter.json";
-import { getZkCred } from "../service/get-zk-cred";
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Noir } from '@noir-lang/noir_js';
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import circuit from '../circuits/target/noirstarter.json';
+import { PassportCred, Proved, ZkSybil } from '@sybil-center/zkc-core';
+import { EthWalletProvider, IEIP1193Provider } from '../service/wallet-provider';
+import { toZkInput } from '../service/tozk-input';
 
 
-type GetZkCredType = Awaited<ReturnType<typeof getZkCred>>
+const sybil = new ZkSybil(new URL('https://api.dev.sybil.center'));
 
 function Component() {
   const [proof, setProof] = useState(Uint8Array.from([]));
+  const [proofVerified, setProofVerified] = useState(false);
   const [noir, setNoir] = useState<Noir | null>(null);
   const [backend, setBackend] = useState<BarretenbergBackend | null>(null);
-  const [credData, setCredData] = useState<GetZkCredType | null>();
+  const [zkCred, setZkCred] = useState<Proved<PassportCred> | null>(null);
+  const [walletProvider, setWalletProvider] = useState<EthWalletProvider | null>(null);
 
-
-  // Calculates proof
   const calculateProof = async () => {
     const calc = new Promise(async (resolve, reject) => {
-      const proof = await noir!.generateFinalProof(credData!.toZkpInput());
-      console.log("Proof created: ", proof);
+      const proof = await noir!.generateFinalProof(toZkInput(zkCred!));
+      console.log('Proof created: ', proof);
       setProof(proof);
       resolve(proof);
     });
     toast.promise(calc, {
-      pending: "Calculating proof...",
-      success: "Proof calculated!",
-      error: "Error calculating proof",
+      pending: 'Calculating proof...',
+      success: 'Proof calculated!',
+      error: 'Error calculating proof',
     });
   };
 
+  function connectWallet() {
+    toast.promise(new Promise(async (resolve, reject) => {
+      const injected = 'ethereum' in window && (window.ethereum as IEIP1193Provider);
+      if (!injected) throw new Error(`No window.ethereum available`);
+      const walletProvider = new EthWalletProvider(injected);
+      await walletProvider.connect();
+      setWalletProvider(walletProvider);
+      resolve(walletProvider);
+    }), {
+      pending: 'Wallet connecting...',
+      success: 'Connected',
+      error: 'Wallet connection failed',
+    });
+  }
+
+  async function getZkCred() {
+    if (!walletProvider) throw new Error(`Connect wallet first`);
+    toast.promise(new Promise(async (resolve) => {
+      const cred = await sybil.credential('passport', await walletProvider.getProof(), {});
+      setZkCred(cred);
+      resolve(cred);
+    }), {
+      pending: 'Getting ZK credential ...',
+      success: 'You got Credential',
+      error: 'Getting credential fail',
+    });
+  }
+
   const verifyProof = async () => {
-    const verifyOffChain = new Promise(async (resolve, reject) => {
+    const verifyOffChain = new Promise(async (resolve) => {
       if (proof) {
         const verification = await noir!.verifyFinalProof(proof);
-        console.log("Proof verified: ", verification);
+        console.log('Proof verified: ', verification);
+        setProofVerified(true);
         resolve(verification);
       }
     });
 
     toast.promise(verifyOffChain, {
-      pending: "Verifying proof off-chain...",
-      success: "Proof verified off-chain!",
-      error: "Error verifying proof",
+      pending: 'Verifying proof off-chain...',
+      success: 'Proof verified off-chain!',
+      error: 'Error verifying proof',
     });
   };
-
-  // Verifier the proof if there's one in state
-  useEffect(() => {
-    if (proof.length > 0) {
-      verifyProof();
-
-      return () => {
-        // TODO: Backend should be destroyed by Noir JS so we don't have to
-        // store backend in state
-        backend!.destroy();
-      };
-    }
-  }, [proof]);
 
   const initNoir = async () => {
     const backend = new BarretenbergBackend(circuit);
     setBackend(backend);
     const noir = new Noir(circuit, backend);
     await toast.promise(noir.init(), {
-      pending: "Initializing Noir...",
-      success: "Noir initialized!",
-      error: "Error initializing Noir",
+      pending: 'Initializing Noir...',
+      success: 'Noir initialized!',
+      error: 'Error initializing Noir',
     });
     setNoir(noir);
   };
 
   useEffect(() => {
     initNoir();
-    (async () => setCredData(await getZkCred()))();
+    return () => {
+      // TODO: Backend should be destroyed by Noir JS so we don't have to
+      // store backend in state
+      backend?.destroy();
+    };
   }, []);
 
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Example starter</h1>
-      <h2>This circuit checks that x and y are different</h2>
-      <p>Try it!</p>
-      <input name="x" type={"number"}/>
-      <input name="y" type={"number"}/>
+      <button onClick={connectWallet}>Connect wallet</button>
+      <button onClick={getZkCred}>Get ZK cred</button>
       <button onClick={calculateProof}>Calculate proof</button>
+      <button onClick={verifyProof}>Verify proof</button>
     </div>
   );
 }
---
+++ service/wallet-provider.ts
@@ -0,0 +1,84 @@
+import * as u8a from 'uint8arrays';
+import { WalletProof, WalletProvider, ZkcId } from '@sybil-center/zkc-core';
+
+export interface RequestArguments {
+  readonly method: string;
+  readonly params?: readonly unknown[] | object;
+}
+
+export interface IEIP1193Provider {
+  enable?: () => Promise<void>;
+  request<T = unknown>(args: RequestArguments): Promise<T>;
+}
+
+export class EthWalletProvider implements WalletProvider {
+  constructor(private readonly provider: IEIP1193Provider) {
+    this.sign = this.sign.bind(this);
+    this.getAddress = this.getAddress.bind(this);
+    this.getSubjectId = this.getSubjectId.bind(this);
+    this.getProof = this.getProof.bind(this);
+  }
+
+  async getSubjectId(): Promise<ZkcId> {
+    const address = await this.getAddress();
+    return {
+      t: 1,
+      k: address,
+    };
+  };
+
+  async getProof(): Promise<WalletProof> {
+    const subjectId = await this.getSubjectId();
+    return {
+      subjectId: subjectId,
+      signFn: this.sign,
+    };
+  };
+
+  async sign(args: { message: string }): Promise<string> {
+    const message = args.message;
+    const address = await this.getAddress();
+    const hex = u8a.toString(u8a.fromString(message), 'hex');
+    return await this.#signMessage(address, hex);
+  }
+
+  async getAddress(): Promise<string> {
+    const accounts = (await this.provider.request<string[]>({
+      method: 'eth_accounts',
+    }));
+    const account = accounts[0];
+    if (!account) {
+      throw new Error(`Enable Ethereum provider`);
+    }
+    return account;
+  }
+
+  /**
+   * Return signature as base64 string
+   * @param address - ethereum 0x<address>
+   * @param hexMessage - message as hex string
+   */
+  async #signMessage(address: string, hexMessage: string): Promise<string> {
+    try {
+      return await this.provider.request<`0x${string}`>({
+          method: 'eth_sign',
+          params: [address, hexMessage],
+        },
+      );
+    } catch (err) {
+      const reason = err as Error;
+      if ('code' in reason && (reason.code === -32602 || reason.code === -32601)) {
+        return await this.provider.request<`0x${string}`>({
+            method: 'personal_sign',
+            params: [address, hexMessage],
+          },
+        );
+      }
+      throw reason;
+    }
+  }
+
+  async connect(): Promise<string> {
+    return this.provider.request<string>({ method: 'eth_requestAccounts' });
+  }
+}
